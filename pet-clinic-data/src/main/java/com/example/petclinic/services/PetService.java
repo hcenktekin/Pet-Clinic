0ed772b5063562404cdf364a2d970aa1d50889e8
havactekin <hava.util@gmail.com>
2022-10-30 17:39:04 +0300

    refactoring to common crud interfaces.

--- pet-clinic-data/src/main/java/com/example/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/PetService.java
@@ -2,14 +2,6 @@ package com.example.petclinic.services;
 
 import com.example.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
